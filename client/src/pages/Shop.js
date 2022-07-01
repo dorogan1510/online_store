fb3bce38176059a65e64d80b163c55f118eea48e
Dan Frolov <frolovdan94@gmail.com>
2022-07-01 18:34:01 +0300

    Feature(Client) Create auth panel
    Add authorization panel for admin and users

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Col from 'react-bootstrap/esm/Col'
+import Container from 'react-bootstrap/esm/Container'
+import TypeBar from './../components/TypeBar'
 
 const Shop = () => {
-    return <div>Shop</div>
+    return (
+        <Container>
+            <div className='mt-2'>
+                <Col md={3}>
+                    <TypeBar />
+                </Col>
+                <Col md={9}></Col>
+            </div>
+        </Container>
+    )
 }
 
 export default Shop
