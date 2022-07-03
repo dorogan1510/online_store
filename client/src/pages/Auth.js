ad409ac4c961769b82754dc340c34867805a5620
Dan Frolov <frolovdan94@gmail.com>
2022-07-03 17:01:15 +0300

    Feature(Client) create first axios parameters

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { NavLink, useLocation } from 'react-router-dom'
 import { LOGIN_ROUTE, REGISTRATION_ROUTE } from './../utils/consts'
+import { login, registration } from './../http/userAPI'
 
 const Auth = () => {
     const location = useLocation()
     const isLogin = location.pathname === LOGIN_ROUTE
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const click = async () => {
+        if (isLogin) {
+            const response = await login()
+        } else {
+            const response = await registration(email, password)
+            console.log(response)
+        }
+    }
     return (
         <Container
             className='d-flex justify-content-center align-items-center'
@@ -22,10 +34,15 @@ const Auth = () => {
                     <Form.Control
                         className='mt-3'
                         placeholder='Введите ваш email'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <Form.Control
                         className='mt-3'
                         placeholder='Введите ваш пароль'
+                        value={password}
+                        type='password'
+                        onChange={e => setPassword(e.target.value)}
                     />
                     <div className='d-flex flex-row justify-content-between mt-3'>
                         {isLogin ? (
@@ -41,7 +58,7 @@ const Auth = () => {
                                 <NavLink to={LOGIN_ROUTE}>Войдите</NavLink>
                             </div>
                         )}
-                        <Button variant={'outline-success'}>
+                        <Button variant={'outline-success'} onClick={click}>
                             {isLogin ? 'Войти' : 'Регистрация'}
                         </Button>
                     </div>
